21d4014e395c91b9882000a2cd5593f6a9a44fac
Francisco <frapagis@hotmail.com>
2018-05-25 19:47:18 +0200

    click move

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,82 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
-    private Animator anim;
-    public float speed = 2;
-    public float maxSpeed = 4;
-    private float currentSpeed;
-    public float timeToMax = 1;
-    public float rotationSpeed = 100;
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private Vector3 currentDestination;
 
-    private float currentTime = 0;
-
-// Use this for initialization
     void Start()
     {
-        anim = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
 // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-// Axes
-        float turning = Input.GetAxis("Horizontal");
-        float moving = Input.GetAxis("Vertical");
-// If idle and turning
-        if (moving == 0)
-        {
-            if (Input.GetKeyDown("right"))
-                anim.SetTrigger("turnRight");
-            if (Input.GetKeyDown("left"))
-                anim.SetTrigger("turnLeft");
-        } // Moving the character
+        _animator.SetFloat("speed", _navMeshAgent.desiredVelocity.magnitude);
 
-        if (moving > 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            transform.Translate(Vector3.forward * moving * currentSpeed * Time.deltaTime);
-            transform.Rotate(Vector3.up * turning * rotationSpeed * Time.deltaTime);
-            anim.SetBool("isWalking", true);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 200.0f, LayerMask.GetMask("Ground")))
+            {
+                if (hit.transform != null)
+                {
+                    _navMeshAgent.speed = 2f;
+
+                    currentDestination = hit.point;
+                    SetDestination();
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 200.0f, LayerMask.GetMask("Ground")))
+            {
+                if (hit.transform != null)
+                {
+                    _navMeshAgent.speed = 4f;
+
+                    currentDestination = hit.point;
+                    SetDestination();
+                }
+            }
         }
 
-        if (moving == 0)
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isWalkingBackwards", false);
-        }
+    }
 
-        if (moving < 0)
+    private void SetDestination()
+    {
+        if (currentDestination != null)
         {
-            transform.Translate(Vector3.forward * moving * speed / 2.0f * Time.deltaTime);
-            transform.Rotate(-Vector3.up * turning * rotationSpeed * Time.deltaTime);
-            anim.SetBool("isWalkingBackwards", true);
+            _navMeshAgent.SetDestination(currentDestination);
         }
-
-// Jumping
-        if (Input.GetButtonDown("Jump") && moving >= 0)
-            anim.SetTrigger("Jump");
-// Speed increment
-        if (currentTime == 0 && moving > 0)
-        {
-            currentSpeed = speed;
-            currentTime = Time.time;
-        }
-
-        if (moving > 0 && currentSpeed < maxSpeed)
-        {
-            currentTime += Time.deltaTime;
-            currentSpeed += timeToMax * Time.deltaTime;
-        }
-
-        if (moving == 0)
-        {
-            currentTime = 0;
-            if (currentSpeed > speed)
-                currentSpeed -= timeToMax * Time.deltaTime * 2.0f;
-        }
-
-        anim.SetFloat("Speed", currentSpeed);
     }
 }
